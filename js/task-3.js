b472b99cd1b0e2f922118eee65cc173861dd97a5
MaryaSamsonenko <84477591+MaryaSamsonenko@users.noreply.github.com>
2021-09-08 16:37:56 +0300

    добавляет 3 скрипта

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,21 @@
 // Напиши фукцнию findLongestWord(string), которая принимает параметром произвольную строку (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
 
-// const findLongestWord = function (string) {
-//  // твой код
-// };
+const findLongestWord = function (string) {
+    let stringArray = string.split(' ');
+    let longestWord = stringArray[0];
+    for (let i = 0; i < stringArray.length; i += 1) {
+        if (longestWord.length < stringArray[i].length) {
+            longestWord = stringArray[i];
+        }
+    }
+    return longestWord;
+};
 
 // /*
 //  * Вызовы функции для проверки работоспособности твоей реализации.
 //  */
-// console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
 
-// console.log(findLongestWord('Google do a roll')); // 'Google'
+console.log(findLongestWord('Google do a roll')); // 'Google'
 
-// console.log(findLongestWord('May the force be with you')); // 'force'
+console.log(findLongestWord('May the force be with you')); // 'force'
